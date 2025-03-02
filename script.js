d3778c9873e9cb6ff57f9bd86ef24b29550311db
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-03-02 15:06:55 +0400

    message

--- script.js
+++ script.js
@@ -7,8 +7,46 @@ let sel = document.querySelector('.sel');
 let sel2 = document.querySelector('.sel2')
 let sel3 = document.querySelector('.sel3')
 let sel4 = document.querySelector('.sel4')
+let productId = window.location.search.split('=')[1]
+
+// let btn = document.querySelector('.order-now')
+
+
 
 document.querySelector('.footer').innerHTML += footer
+
+
+
+
+// let newProduct = []
+
+// btn.addEventListener('click', function (e) {
+//     e.preventDefault()
+//     let btnData = {
+//         quantity: 1,
+//         price: 10,
+//         productId: productId
+//     }
+
+//     fetch(`https://restaurant.stepprojects.ge/api/Baskets/AddToBasket`, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+
+//         },
+//         body: JSON.stringify(btnData)
+//     }).then(resp => resp.json())
+//     .then(resp => {
+//         newProduct.push(resp)
+//     })
+//     console.log(newProduct)
+// })
+
+
+
+
+
+
 
 
 
@@ -27,21 +65,21 @@ function fetchCategories() {
 
 function filterArray(addFilter) {
     if (addFilter.value == "All") {
-        delete filtersArray[addFilter.key] 
-    }
-     
-    else {
-        
-        filtersArray[addFilter.key] = addFilter.value  
+        delete filtersArray[addFilter.key]
     }
 
-    let url = `https://restaurant.stepprojects.ge/api/Products/GetFiltered`; 
+    else {
+
+        filtersArray[addFilter.key] = addFilter.value
+    }
+
+    let url = `https://restaurant.stepprojects.ge/api/Products/GetFiltered`;
 
     url += '?' + Object.entries(filtersArray).map(([key, value]) => `${key}=${value}`).join('&');
 
     fetch(url)
-    .then(resp => resp.json())
-    .then(resp => renderProducts(resp))
+        .then(resp => resp.json())
+        .then(resp => renderProducts(resp))
 }
 
 
@@ -72,9 +110,6 @@ function fillOptions(categories) {
 
 
 
-
-
-
 function renderProducts(products) {
     cont.innerHTML = '';
     for (let product of products) {
@@ -85,18 +120,60 @@ function renderProducts(products) {
                 <h3>Price: ${product.price}</h3>
                 <h4>Nuts: ${product.nuts}</h4>
                 <h3>Spiciness: ${product.spiciness}</h3>
-                <h4>Vegetarian: ${product.vegeterian}</h4>
-                <a href="details.html?id=${product.id}">See Details</a>
+                <h4>Vegetarian: ${product.vegetarian}</h4>
+
+                <div class="btn-parent">
+                    <a href="details.html?id=${product.id}">See Details</a>
+                    <!-- Order Now Button -->
+                    <button class="order-now" data-product-id="${product.id}" data-price="${product.price}">Order Now</button>
+                </div>
             </div>
         `;
     }
+
+
+
+    const btns = document.querySelectorAll('.order-now');
+    
+    btns.forEach(btn => {
+        btn.addEventListener('click', function (e) {
+            e.preventDefault();
+
+
+            const productId = btn.getAttribute('data-product-id');
+            const price = btn.getAttribute('data-price');
+
+            let btnData = {
+                quantity: 1,
+                price: price,
+                productId: productId
+            };
+
+            fetch('https://restaurant.stepprojects.ge/api/Baskets/AddToBasket', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(btnData)
+            })
+                .then(resp => resp.json())
+                .then(resp => {
+                    console.log('Product added to basket:', resp);
+                })
+        });
+    });
 }
+
+
+
+
+
 
 sel.addEventListener("change", function () {
     if (sel.value === "All") {
         getAll();
         filtersArray = {}
-        sel2.value = "All"   
+        sel2.value = "All"
         sel3.value = "All"
         sel4.value = "All"
 
@@ -112,7 +189,7 @@ sel.addEventListener("change", function () {
 sel2.addEventListener("change", function () {
     filterArray({
         key: "vegeterian",
-        value: sel2.value, 
+        value: sel2.value,
     });
 })
 
@@ -136,6 +213,34 @@ fetchCategories()
 getAll()
 
 
+
+
+
+// const popup = document.getElementById('popup');
+// const closePopupBtn = document.getElementById('closePopup');
+// const closePopupBtn2 = document.getElementById('closePopupBtn');
+
+// function showPopup() {
+//   popup.style.display = 'flex';
+// }
+
+
+// setTimeout(showPopup, 3000);
+
+// closePopupBtn.onclick = function() {
+//   popup.style.display = 'none';
+// }
+
+
+// closePopupBtn2.onclick = function() {
+//   popup.style.display = 'none';
+// }
+
+// window.onclick = function(event) {
+//   if (event.target === popup) {
+//     popup.style.display = 'none';
+//   }
+// }
 
 
 
